45b21aca5c3e2b5a6bc7a592d839ee0398913a04
alexeimowat <56664562+alexeimowat@users.noreply.github.com>
2022-07-26 23:11:59 -0600

    displaying recipe title, image, and ingredient, but trying to store all ingredients in an array in the state

--- src/components/recipes/NewRecipe.js
+++ src/components/recipes/NewRecipe.js
@@ -9,11 +9,15 @@ class NewRecipe extends React.Component {
     
     constructor(props) {
         super(props);
-        this.state = {ingredient: " "};
+        this.state = {ingredient: '', recipeImage: '', recipeTitle: '', ingredientList: []};
 
         this.getRecipe = this.getRecipe.bind(this);
     }
     
+    ingredientsElement() {
+        console.log(this.state.ingredientList);
+    }
+
     /* Gets a random recipe from Spoonacular API and updates the state to show the new ingredient
     */
     getRecipe() {
@@ -22,8 +26,13 @@ class NewRecipe extends React.Component {
         // console.log(this.state.ingredient);
         axios.get('https://api.spoonacular.com/recipes/random?apiKey=a1d424f72e474e8db3dec5e0baf85ba3')
             .then((response) => {
-              console.log(response);
-              this.setState({ingredient: response.data.recipes[0].extendedIngredients[0].original})
+                console.log(response.data.recipes[0]);
+                let recipeData = response.data.recipes[0];
+                this.setState({ingredient: recipeData.extendedIngredients[0].original, 
+                                recipeImage: recipeData.image,
+                                recipeTitle: recipeData.title,
+                                ingredientList: recipeData.extendedIngredients});
+                this.ingredientsElement();
             })
            .catch((error)=>{
               console.log(error);
@@ -33,6 +42,8 @@ class NewRecipe extends React.Component {
     render() {
         return (
             <div>
+                <h1>{this.state.recipeTitle}</h1>
+                <img src={this.state.recipeImage} alt="image" width="300" height="200"/>
                 <p>{this.state.ingredient}</p>
                 <button onClick={this.getRecipe}>
                     New Recipe
