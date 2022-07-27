33eb63fc85f89c5003e0f9a309b26b5222b011ae
alexeimowat <56664562+alexeimowat@users.noreply.github.com>
2022-07-27 12:29:52 -0600

    fixed the issue where state wasn't being updated correctly, just moved the logic into the get request where all the other setState calls are made

--- src/components/recipes/NewRecipe.js
+++ src/components/recipes/NewRecipe.js
@@ -5,7 +5,19 @@ displays it to the screen
 import React from 'react';
 import axios, { Axios } from 'axios';
 
-const test = "hello, I am a const";
+// constants
+const initialState = {
+    ingredient: "", // just for testing output, not in final code
+    recipeImage: "",
+    recipeTitle: "",
+    ingredientList: "",
+    instructions: ""
+}
+
+const spoonacularAddr = "https://api.spoonacular.com/recipes/random?apiKey=a1d424f72e474e8db3dec5e0baf85ba3";
+const rapidAPIAddr = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random";
+const rapidAPIKey = "e80a8a6f02msh7a2a36a879b4d5bp1e6ce2jsn80ba9331c81b";
+
 class NewRecipe extends React.Component {
     
 
@@ -17,33 +29,9 @@ class NewRecipe extends React.Component {
                         ingredientList: ['']};
 
         this.getRecipe = this.getRecipe.bind(this);
-        this.ingredientsElement = this.ingredientsElement.bind(this);
-    }
-    
-    ingredientsElement(responseArray) {
-        console.log(responseArray);
-
-        // First we want to replace the old recipe with a blank array
-        let newEmpty = [];
-        this.setState({
-            ingredientList: [...this.state.ingredientList, newEmpty]
-        });
-
-        // next we want to add all the ingredients to a temp array and change the state to 
-        // reflect this new array
-        let tempArray = [];
-        // console.log("Printing all items:");
-        for (let i=0; i<responseArray.length; i++) {
-            // console.log(responseArray[i].original);
-            tempArray[i] = responseArray[i].original;
-        }
-        // console.log("The temp array");
-        // console.log(tempArray);
-
-        this.setState({
-            ingredientList: [...this.state.ingredientList, tempArray]
-        });
-        console.log(this.state.ingredientList);
+        // this.ingredientsElement = this.ingredientsElement.bind(this);
+        this.clearState = this.clearState.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     /* Gets a random recipe from Spoonacular API and updates the state to show the new ingredient
@@ -52,37 +40,62 @@ class NewRecipe extends React.Component {
         // console.log("Calling getRecipe");
         // console.log("Current Value of state:");
         // console.log(this.state.ingredient);
-        axios.get('https://api.spoonacular.com/recipes/random?apiKey=a1d424f72e474e8db3dec5e0baf85ba3')
+        axios.get(spoonacularAddr)
             .then((response) => {
                 console.log(response.data.recipes[0]);
                 let recipeData = response.data.recipes[0];
+                console.log(recipeData.extendedIngredients.length);
+
+                let tempAr = [];
+                for (let i = 0; i<recipeData.extendedIngredients.length; i++) {
+                    tempAr[i] = recipeData.extendedIngredients[i].original;
+                }
+
                 this.setState({ingredient: recipeData.extendedIngredients[0].original, 
                                 recipeImage: recipeData.image,
                                 recipeTitle: recipeData.title,
+                                ingredientList: tempAr
                                 });
-                this.ingredientsElement(recipeData.extendedIngredients);
-                
-                // console.log(this.state.ingredientList);
             })
            .catch((error)=>{
               console.log(error);
             }); 
-        // console.log(this.state.ingredientList);
-    }  
+    }
+    
+    clearState() {
+        this.setState({...initialState})
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        
+        this.clearState();
+        this.getRecipe();
+        
+    }
     
     render() {
         return (
             <div>
-                <h1>{this.state.recipeTitle}</h1>
-                <img src={this.state.recipeImage} alt="image" width="300" height="200"/>
-                <p>{this.state.ingredient}</p>
-                {/* <div>
-                    {this.state.instructions}
-                </div> */}
-                <button onClick={this.getRecipe}>
-                    New Recipe
-                </button>
+                <form onSubmit={this.handleSubmit}>
+                    <img src={this.state.recipeImage} alt="image" width="300" height="200"/>
+                    <h1>{this.state.recipeTitle}</h1>
+                    {/* <p>{this.state.ingredientList[0]}</p> */}
+                    <p>{this.state.ingredientList[0]}</p>
+                    <p>{this.state.ingredientList[1]}</p>
+                    <button type='submit'>Submit</button>
+                </form>
             </div>
+            // <div>
+            //     <h1>{this.state.recipeTitle}</h1>
+            //     <p>{this.state.ingredient}</p>
+            //     {/* <div>
+            //         {this.state.instructions}
+            //     </div> */}
+            //     <button onClick={this.getRecipe}>
+            //         New Recipe
+            //     </button>
+            // </div>
         );
     }
 }
